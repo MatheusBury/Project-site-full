b6c453aa7d75b4237bab510534c3f26f9a2199c4
Matheus Bury <matheusbury007@gmail.com>
2025-02-10 21:08:15 -0300

    Create Sprint Boot

---
+++ projec-Z/src/main/java/com/bury/projec_Z/ProjecZApplication.java
@@ -0,0 +1,13 @@
+package com.bury.projec_Z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecZApplication.class, args);
+	}
+
+}
---
+++ projec-Z/src/test/java/com/bury/projec_Z/ProjecZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bury.projec_Z;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
